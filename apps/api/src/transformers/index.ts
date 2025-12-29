dfd8346c59bf05de74af3343b42eeed4fff94930
Elishaokon13 <eliscodavid@gmail.com>
2025-12-29 02:32:53 +0100

    Modified apps/api/src/transformers/index.ts

--- apps/api/src/transformers/index.ts
+++ apps/api/src/transformers/index.ts
@@ -6,4 +6,5 @@ export * from './wallet-transformer.js';
 export * from './payout-transformer.js';
 export * from './transaction-transformer.js';
 export * from './webhook-transformer.js';
+export * from './identity-transformer.js';
 
